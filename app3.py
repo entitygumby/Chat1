9e294223bb96153192141a880e5e7fa7989ee781
entitygumby <89120633+entitygumby@users.noreply.github.com>
2023-05-12 12:53:58 +1000

    added chat2 and chat3

---
+++ app3.py
@@ -0,0 +1,44 @@
+import chat3
+
+import streamlit as st
+from streamlit_chat import message
+
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+# Define a function to clear the input text
+def clear_input_text():
+    global input_text
+    input_text = ""
+
+# We will get the user's input by calling the get_text function
+def get_text():
+    global input_text
+    input_text = st.text_input("Ask A Question", key="input", on_change=clear_input_text, label_visibility="hidden")
+    return input_text
+
+def main():
+    user_input = get_text()
+
+    if user_input:
+        output = chat3.answer(user_input)
+#        message(avatar_style=)
+        # store the output
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output)
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+# Run the app
+
+if __name__ == "__main__":
+    main()
+
